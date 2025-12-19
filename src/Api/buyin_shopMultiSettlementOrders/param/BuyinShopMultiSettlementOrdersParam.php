1561229a99d36f77d174c38cb3a55a8eded2e7b2
west_ng <457395070@qq.com>
2025-12-19 14:44:03 +0800

    feat: 新增 4 个 buyin/alliance 相关接口
    - 新增 alliance_getOrderList 联盟订单列表接口
    - 新增 buyin_instituteOrderPick 机构订单挑选接口
    - 新增 buyin_queryShopAllianceOrder 查询店铺联盟订单接口
    - 新增 buyin_shopMultiSettlementOrders 店铺多结算订单接口

---
+++ src/Api/buyin_shopMultiSettlementOrders/param/BuyinShopMultiSettlementOrdersParam.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of DouDian-SDK
+ *
+ * @link     https://github.com/westng/doudian-sdk-php
+ * @document https://github.com/westng/doudian-sdk/blob/main/README.md
+ * @contact  457395070@qq.com
+ * @license  https://github.com/westng/doudian-sdk/blob/main/LICENSE
+ */
+
+namespace DouDianSdk\Api\buyin_shopMultiSettlementOrders\param;
+
+// auto generated code
+class BuyinShopMultiSettlementOrdersParam
+{
+    public $size;
+
+    public $cursor;
+
+    public $time_type;
+
+    public $start_time;
+
+    public $end_time;
+
+    public $order_ids;
+}
